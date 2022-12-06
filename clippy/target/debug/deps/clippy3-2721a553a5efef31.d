9a918d4b8898c44753bb12d55696bc75349b9317
raymond <raymond@thenetcircle.com>
2022-12-06 11:14:13 +0800

    my solution of rustling

---
+++ clippy/target/debug/deps/clippy3-2721a553a5efef31.d
@@ -0,0 +1,7 @@
+/Users/raymond/Code/rustlings/exercises/clippy/target/debug/deps/clippy3-2721a553a5efef31.rmeta: clippy3.rs
+
+/Users/raymond/Code/rustlings/exercises/clippy/target/debug/deps/clippy3-2721a553a5efef31.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
